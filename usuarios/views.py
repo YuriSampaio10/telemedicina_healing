c533c3aab908e4b68cc902b148fb236d667cbe85
Yuri Sampaio <sampaio.yuri31@gmail.com>
2024-04-21 16:43:08 -0300

    capturando o que foi digitado nos inputs do cadastro.html e adicionando condições casa a senha seja diferente de confirmar_senha

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def cadastro(request):
-    return render(request, "cadastro.html")
+    print(request.method)
+    if request.method == "GET":
+        return render(request, "cadastro.html")
+    elif request.method == "POST":
+        # pega os dados do dicionario post do campo username la do cadastro.html
+        username = request.POST.get("username")
+        # pega os dados do dicionario post do campo email la do cadastro.html
+        email = request.POST.get("email")
+        # pega os dados do dicionario post do campo senha la do cadastro.html
+        senha = request.POST.get("senha")
+        # pega os dados do dicionario post do campo confirmar_senha la do cadastro.html
+        confirmar_senha = request.POST.get("confirmar_senha")
+
+        # confirmar se a senha e a confirmar_senha são iguais
+        if senha != confirmar_senha:
+            print("erro, senhas diferentes")
+            return redirect("/usuarios/cadastro")
+
+        return HttpResponse(f"{username}- {email}- {senha}- {confirmar_senha}")
