6dcadde2080406e4b52244ced28173596e3e59e2
Yuri Sampaio <sampaio.yuri31@gmail.com>
2024-04-21 20:53:11 -0300

    adicionando a tabela especialidades do BD para o django admin inplementar

--- medico/admin.py
+++ medico/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Especialidades
 
-# Register your models here.
+
+# registra no site, na area administrative a model especialidade
+admin.site.register(Especialidades)
