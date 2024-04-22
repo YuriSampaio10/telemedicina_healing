55ad719d56461fe5ed2c63e79fb7c05a0adea5a9
Yuri Sampaio <sampaio.yuri31@gmail.com>
2024-04-21 21:14:47 -0300

    adicionando mais campos a tabela DadosMedicos

--- medico/models.py
+++ medico/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -18,8 +19,16 @@ class DadosMedico(models.Model):
     rua = models.CharField(max_length=15)
     bairro = models.CharField(max_length=15)
     numero = models.IntegerField()
+    # o rg, cim e foto de perfil sao imagens que o campo vai receber, upload_to vai para a pasta /media/ que a gente ja configurou no settings.py no core do projeto e em cada pasta tera uma outra pasta com nome diferente 
     rg = models.ImageField(upload_to="rgs")
-    cedulas_identidade_medica= models.ImageField(upload_to="cim")
+    cedulas_identidade_medica = models.ImageField(upload_to="cim")
     foto = models.ImageField(upload_to="fotos_perfil")
     descricao = models.TextField()
     valor_consulta = models.FloatField(default=100)
+    # pega da chave estrangeira para a relação do usuario do banco de dados que esta no auth.user com dados medicos
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+
+    # Pesquisar
+    def __str__(self):
+            return self.user.username
