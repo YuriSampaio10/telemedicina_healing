8238e4ab48e12b880b8cb6de74d9a3e3c1b74efb
Yuri Sampaio <sampaio.yuri31@gmail.com>
2024-04-21 21:17:54 -0300

    adicionando a tabela no BD e colocando ela no admin.py para usala na area administrativa do django

--- medico/admin.py
+++ medico/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Especialidades
+from .models import Especialidades, DadosMedico
 
 
 # registra no site, na area administrative a model especialidade
 admin.site.register(Especialidades)
+admin.site.register(DadosMedico)
