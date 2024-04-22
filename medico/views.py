72c60d0c15ca3f41940c3c008a181d676509d95e
Yuri Sampaio <sampaio.yuri31@gmail.com>
2024-04-22 00:15:41 -0300

    pegando os dados do cadastro_medico.html para a views.py do app medico

--- medico/views.py
+++ medico/views.py
@@ -11,3 +11,28 @@ def cadastro_medico(request):
         return render(
             request, "cadastro_medico.html", {"especialidades": especialidades}
         )
+    elif request.method == "POST":
+        # pega os dados do dicionario post do campo crm la do cadastro_medico.html
+        crm = request.POST.get("crm")
+        # pega os dados do dicionario post do campo nome la do cadastro_medico.html
+        nome = request.POST.get("nome")
+        # pega os dados do dicionario post do campo cep la do cadastro_medico.html
+        cep = request.POST.get("cep")
+        # pega os dados do dicionario post do campo rua la do cadastro_medico.html
+        rua = request.POST.get("rua")
+        # pega os dados do dicionario post do campo bairro la do cadastro_medico.html
+        bairro = request.POST.get("bairro")
+        # pega os dados do dicionario post do campo numero la do cadastro_medico.html
+        numero = request.POST.get("numero")
+        # pega os dados do dicionario post do campo cim la do cadastro_medico.html
+        cim = request.FILES.get("cim")
+        # pega os dados do dicionario post do campo rg la do cadastro_medico.html
+        rg = request.FILES.get("rg")
+        # pega os dados do dicionario post do campo foto la do cadastro_medico.html
+        foto = request.FILES.get("foto")
+        # pega os dados do dicionario post do campo especialidade la do cadastro_medico.html
+        especialidade = request.POST.get("especialidade")
+        # pega os dados do dicionario post do campo descricao la do cadastro_medico.html
+        descricao = request.POST.get("descricao")
+        # pega os dados do dicionario post do campo valor_consulta la do cadastro_medico.html
+        valor_consulta = request.POST.get("valor_consulta")
